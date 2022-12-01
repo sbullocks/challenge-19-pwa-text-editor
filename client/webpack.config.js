ed2aa7309cf1b03c96e0ae0ed6427d8de26380f9
sbullocks <sbullocks@gmail.com>
2022-12-01 16:06:03 -0500

    added webpack plugin that inject custom service worker.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,6 +24,12 @@ module.exports = () => {
         template: './index.html',
         title: 'Contact Cards'
       }),
+
+       // Injects our custom service worker
+       new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
       
     ],
 
