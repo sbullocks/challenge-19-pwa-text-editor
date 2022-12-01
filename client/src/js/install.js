0de4d9ed448c8aa252e1dd3ec62d3052f31f31c6
sbullocks <sbullocks@gmail.com>
2022-12-01 16:45:42 -0500

    correction.. added event handler to the beforeinstallprompt event.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -6,9 +6,10 @@ window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
   });
-  
+
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
+
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
