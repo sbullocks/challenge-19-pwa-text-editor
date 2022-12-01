9f06d25e882f2c3626d6109b6f318c08634f8eb3
sbullocks <sbullocks@gmail.com>
2022-12-01 17:14:46 -0500

    added the get confirmation of the request.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -54,5 +54,10 @@ export const getDb = async () => {
   // Use the .getAll() method to get all data in the database.
     const request = store.getAll();
 
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
