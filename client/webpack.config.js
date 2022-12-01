b70f560ad23094168416788827a506440dc6114d
sbullocks <sbullocks@gmail.com>
2022-12-01 16:08:19 -0500

    added rules to webpacks.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,8 +34,24 @@ module.exports = () => {
     ],
 
     module: {
+      // CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
