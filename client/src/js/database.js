6181a174a8056234062954e632c9f854b0744130
sbullocks <sbullocks@gmail.com>
2022-12-01 16:49:25 -0500

    added comments to database.js file to explain code.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,12 +1,15 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+// We are creating a new database named 'contact' which will be using version 1 of the database.
   openDB('jate', 1, {
+    // Add our database schema if it has not already been initialized.
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
