fec5879aeacaf2807983c2f91084750f1762c0cd
sbullocks <sbullocks@gmail.com>
2022-12-01 16:45:55 -0500

    implemented click event handler on the butInstall element.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,8 +8,20 @@ window.addEventListener('beforeinstallprompt', (event) => {
   });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
-
+butInstall.addEventListener('click', async () => {
+  
+    const promptEvent = window.deferredPrompt;
+  
+    if (!promptEvent) {
+     return;
+    }
+  
+    promptEvent.prompt();
+    
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+  });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
