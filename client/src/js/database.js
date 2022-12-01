a7ad418e704bd783031d2decb093c22afde5f88e
sbullocks <sbullocks@gmail.com>
2022-12-01 17:09:47 -0500

    fixed syntax.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,19 +17,26 @@ const initdb = async () =>
 
 // Export a function we will use to POST to the database.
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
 
-// Create a connection to the database database and version we want to use.
-const contactDb = await openDB('contact', 1);
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('contact', 1);
 
-// Create a new transaction and specify the database and data privileges.
-const tx = contactDb.transaction('contact', 'readwrite');
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('contact', 'readwrite');
 
- // Open up the desired object store.
- const store = tx.objectStore('contact');
+  // Open up the desired object store.
+  const store = tx.objectStore('contact');
 
   // Use the .add() method on the store and pass in the content.
   const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+;
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
