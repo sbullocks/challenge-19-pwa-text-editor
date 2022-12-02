45dfc3bfc23bd1249154fe49602da07ef2797adb
sbullocks <sbullocks@gmail.com>
2022-12-01 21:03:40 -0500

    fixed dist issue.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,16 +2,16 @@ import { openDB } from 'idb';
 
 const initdb = async () =>
 // We are creating a new database named 'contact' which will be using version 1 of the database.
-  openDB('jate', 1, {
+  openDB('JATE', 1, {
     // Add our database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('JATE')) {
+        console.log('JATE database already exists');
         return;
       }
       // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('JATE', { keyPath: 'id', autoIncrement: true });
+      console.log('JATE database created');
     },
   });
 
@@ -21,13 +21,13 @@ export const putDb = async (content) => {
   console.error('putDb not implemented');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const contactDb = await openDB('JATE', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = contactDb.transaction('JATE', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('jate');
+  const store = tx.objectStore('JATE');
 
   // Use the .add() method on the store and pass in the content.
   const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
@@ -43,13 +43,13 @@ export const getDb = async () => {
   console.error('getDb not implemented');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const contactDb = await openDB('JATE', 1);
 
   // Create a new transaction and specify the database and data privileges.
-    const tx = contactDb.transaction('jate', 'readonly');
+    const tx = contactDb.transaction('JATE', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('jate');
+  const store = tx.objectStore('JATE');
 
   // Use the .getAll() method to get all data in the database.
     const request = store.getAll();
